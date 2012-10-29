01df37376366a36070fb88aad82361a526f9ebc5
Ivan Gonzalez <iglpdc@gmail.com>
2012-10-29 11:55:02 -0400

    Ridiculous fix: one hour lost.

---
+++ tests/__init__.py
@@ -0,0 +1,9 @@
+'''
+File: __init__.py
+Author: Ivan Gonzalez
+Description: Imports the dmrg101 module
+'''
+import os
+import sys
+dmrg101_module_path=os.path.abspath('../dmrg101')
+sys.path.append(dmrg101_module_path)
