efc08ec011b3a0fc9559d6515d09dec6df3a6ce6
Ivan Gonzalez <iglpdc@gmail.com>
2012-10-31 17:46:23 -0400

    Fixing the docs.

--- dmrg101/core/braket.py
+++ dmrg101/core/braket.py
@@ -1,39 +1,55 @@
-'''
-File: braket.py
-Author: Ivan Gonzalez
-Description: A function to implement quantum-mechanics brakets
-'''
+#
+# File : braket.py
+# Author : Ivan Gonzalez
+#
+"""A module to implement quantum-mechanics brakets.
+"""
 from numpy import inner, conjugate
 from dmrg_exceptions import DMRGException
 
 def braket(bra, ket):
-    """Takes a bra and a ket and return their braket
+    """Takes a bra and a ket and return their braket.
 
     You use this function to calculate the quantum mechanical braket, i.e.
     the inner product in the wavefunction Hilbert space of two
-    wavefunction.
+    wavefunctions.
 
     The wavefunction in the bra is hermitian conjugated by the braket
     function.
 
     Parameters
     ----------
-        bra: a Wavefunction with the bra part of the braket.
-        ket: a Wavefunction with the ket part of the braket.
+    bra : a Wavefunction 
+        the bra part of the braket.
+    ket : a Wavefunction 
+	the ket part of the braket.
 
     Returns
     -------
-        a double/complex with value of the braket.
+    result : a double/complex depending on the type of the wavefuntions
+        the value of the braket.
  
     Raises
     ------
-        DMRGException: if the wavefunction don't belong to the same
-	    Hilbert space, i.e. they have a different number of elements.
+    DMRGException
+        if the wavefunction don't belong to the same Hilbert space,
+        i.e. they have a different number of elements.
+
+    Examples
+    --------
+    >>> from dmrg101.core.wavefunction import Wavefunction
+    >>> from dmrg101.core.braket import braket
+    >>> bra = Wavefunction(2,1)
+    >>> bra.randomize()
+    >>> ket = Wavefunction(2,1)
+    >>> ket.randomize()
+    >>> print bra, ket
+    >>> print braket(bra, ket)
     """
     # use wf.as_matrix to access the matrix elements of wf
     if bra.as_matrix.shape() != ket.as_matrix.shape():
-	raise DMRGException("Wavefunctions in braket are not in the same
-	    		     Hilbert space")
+	raise DMRGException("Wavefunctions are not in the same Hilbert space")
 
     hermitian_conjugated_bra=conjugate(bra.as_matrix).transpose()
-    return inner(hermitian_conjugated_bra, ket.as_matrix)
+    result = inner(hermitian_conjugated_bra, ket.as_matrix)
+    return result
