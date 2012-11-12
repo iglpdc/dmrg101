c2dccc050465a3f71eb6db5cb8914b49a3f14b74
Ivan Gonzalez <iglpdc@gmail.com>
2012-11-12 13:55:35 -0500

    Moving to numpy as much as I could and some fixes.

--- dmrg101/utils/tridiagonal_solver/inversePower3.py
+++ dmrg101/utils/tridiagonal_solver/inversePower3.py
@@ -1,38 +1,74 @@
-## module inversePower3
-''' lam,x = inversePower3(d,c,s,tol=1.0e-6).
+# 
+# File: tridiagonal_exceptions.py
+# Author: Ivan Gonzalez
+# 
+"""Inverse power method applied to a tridiagonal matrix.
+
+Adapted from Jaan Kiusalaas, Numerical Methods in Engineering with Python.
+"""
+import numpy as np
+from LUdecomp3 import *
+from tridiagonal_exceptions import TridiagonalException
+
+def inversePower3(d, c, s, tol=1.0e-6, max_iterations=100):
+    """ Inverse power method for triagonal matrix.
+
     Inverse power method applied to a tridiagonal matrix
     [A] = [c\d\c]. Returns the eigenvalue closest to 's'
     and the corresponding eigenvector.
-'''
-from numpy import dot,zeros
-from LUdecomp3 import *
-from math import sqrt
-from random import random
 
-def inversePower3(d,c,s,tol=1.0e-6):
+    Parameters
+    ----------
+    d : a numpy array of ndim = 1.
+        The matrix in the diagonal.
+    c : a numpy array of ndim = 1.
+        The matrix in the second-diagonal.
+    s : a double.
+        The eigenvalue you want to find its eigenvector. This is an
+	approximate value.
+    tol : a double (optional).
+        The overlap between consecutive solutions has to be smaller than
+	that to call the eigenvector converged.
+    max_iterations : an int.
+        The maximum number of iteration done before raising an exception.
+
+    Returns
+    -------
+    s : a double.
+        The eigenvalue corrected.
+    x : a numpy array of ndim = 1. 
+        The corresponding eigenvector.
+
+    Raises
+    ------
+    DMRGException 
+        if the number of iterations before converging exceeds
+	`max_iterations`.
+    """
     n = len(d)
     e = c.copy()
-    cc = c.copy()               # Save original [c]
-    dStar = d - s               # Form [A*] = [A] - s[I]
-    LUdecomp3(cc,dStar,e)       # Decompose [A*]
-    x = zeros(n)
-    for i in range(n):          # Seed [x] with random numbers
-        x[i] = random()
-    xMag = sqrt(dot(x,x))       # Normalize [x]
-    x =x/xMag
-    flag = 0
-    for i in range(30):         # Begin iterations    
-        xOld = x.copy()         # Save current [x]
-        LUsolve3(cc,dStar,e,x)  # Solve [A*][x] = [xOld]
-        xMag = sqrt(dot(x,x))   # Normalize [x]
-        x = x/xMag
-        if dot(xOld,x) < 0.0:   # Detect change in sign of [x]
+    cc = c.copy()                 # Save original [c]
+    dStar = d - s                 # Form [A*] = [A] - s[I]
+    LUdecomp3(cc,dStar,e)         # Decompose [A*]
+    x = np.random.rand(n)         # Seed [x] with random numbers
+    norm = np.linalg.norm(x)      # Normalize [x]
+    x /= norm
+    is_converged = False
+    iteration = 0
+    sign = 1.0
+    while not is_converged:       # Begin iterations    
+	iteration +=1
+	if iteration > max_iterations:
+	    raise TridiagonalException('Inverse power method did not converge')
+        xOld = x.copy()           # Save current [x]
+        LUsolve3(cc,dStar,e,x)    # Solve [A*][x] = [xOld]
+        norm = np.linalg.norm(x)  # Normalize [x]
+        x /= norm
+        if np.dot(xOld,x) < 0.0:     # Detect change in sign of [x]
             sign = -1.0
             x = -x
-        else: sign = 1.0
-        if sqrt(dot(xOld - x,xOld - x)) < tol:
-            return s + sign/xMag,x
-    print 'Inverse power method did not converge'
-
-    
-    
+	overlap = np.linalg.norm(xOld - x)
+        if overlap < tol:
+	    is_converged =True
+    s += sign/norm
+    return s, x
