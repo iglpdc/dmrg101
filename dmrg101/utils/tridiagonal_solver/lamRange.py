11c23dbabbb2293ad3612e7673a5e8bee87161af
Ivan Gonzalez <iglpdc@gmail.com>
2012-11-12 13:57:07 -0500

    Minor clean-up.

--- dmrg101/utils/tridiagonal_solver/lamRange.py
+++ dmrg101/utils/tridiagonal_solver/lamRange.py
@@ -4,30 +4,30 @@
     separates the N lowest eigenvalues of the tridiagonal
     matrix [A] = [c\d\c]; that is, r[i] < lam[i] < r[i+1].
 '''
-from numpy import ones
+import numpy as np
 from sturmSeq import *
 from gerschgorin import *
 
-def lamRange(d,c,N):
-    lamMin,lamMax = gerschgorin(d,c)
-    r = ones(N+1)
+def lamRange(d, c, N):
+    lamMin,lamMax = gerschgorin(d, c)
+    r = np.empty(N+1)
     r[0] = lamMin
-  # Search for eigenvalues in descending order  
+    # Search for eigenvalues in descending order  
     for k in range(N,0,-1):
-      # First bisection of interval(lamMin,lamMax)
+        # First bisection of interval(lamMin,lamMax)
         lam = (lamMax + lamMin)/2.0
         h = (lamMax - lamMin)/2.0
         for i in range(1000):
-          # Find number of eigenvalues less than lam
+            # Find number of eigenvalues less than lam
             p = sturmSeq(d,c,lam)
             numLam = numLambdas(p)
-          # Bisect again & find the half containing lam 
+            # Bisect again & find the half containing lam 
             h = h/2.0
             if numLam < k: lam = lam + h
             elif numLam > k: lam = lam - h
             else: break
-      # If eigenvalue located, change the upper limit
-      # of search and record it in [r]
+        # If eigenvalue located, change the upper limit
+        # of search and record it in [r]
         lamMax = lam
         r[k] = lam
     return r
