523fdf097f5dfac9fea3fd646f868b81658a527d
Ivan Gonzalez <iglpdc@gmail.com>
2012-11-12 14:02:39 -0500

    Using the tridiagonal solver module.

--- dmrg101/utils/tridiagonal_solver/example_9_13.py
+++ dmrg101/utils/tridiagonal_solver/example_9_13.py
@@ -1,16 +1,10 @@
 #!/usr/bin/env python
 from numpy import ones 
-from lamRange import *
-from inversePower3 import *
-N = 100
-n = 100
+from tridiagonal_solver import *
+n = 300
 d = ones((n))*2.0
 c = ones((n-1))*(-1.0)
-r = lamRange(d,c,N)
-print r
-for i in range(1, len(r)):
-    s = (r[i-1] + r[i])/2.0
-    print s
-#    lam,x = inversePower3(d,c,s) # Inverse power method 
-#    print "Eigenvalue No." , i, " =",lam
-#    #print "Eigenvec No." , i, " =",x
+
+evals, evecs = tridiagonal_solver(d,c) 
+print evals
+print evecs
