9fcebf0988d163cc1827f2a38b7135037fb26a59
Ivan Gonzalez <iglpdc@gmail.com>
2012-11-12 16:31:08 -0500

    Adding an exception.

--- dmrg101/utils/tridiagonal_solver/tridiagonal_solver.py
+++ dmrg101/utils/tridiagonal_solver/tridiagonal_solver.py
@@ -1,9 +1,10 @@
 import numpy as np
+from tridiagonal_exceptions import TridiagonalException
 from lamRange import *
 from inversePower3 import *
 from eigenvals3 import *
 
-def tridiagonal_solver(d, c, eigenvectors = True):
+def tridiagonal_solver(d, e, eigenvectors = True):
     """Calculates the eigenvalues and eigenvectors of a tridiagonal and
     symmetric matrix.
 
@@ -22,27 +23,34 @@ def tridiagonal_solver(d, c, eigenvectors = True):
         The eigenvalues.
     evecs : a numpy array with ndim = 2.
         The eigenvectors.
+
+    Raises
+    ------
+    TridiagonalException 
+        if `d` and `e` have different sizes.
     """
+    if (d.size != e.size):
+        raise TridiagonalException("d, and e have different sizes")
     num_evals = d.size
     evals = np.empty(num_evals)
     evecs = np.empty((num_evals, num_evals))
 
-    r = lamRange(d, c, num_evals)
+    r = lamRange(d, e, num_evals)
     assert(len(r) == num_evals +1)
 
-    evals = eigenvals3(d, c, num_evals)
+    evals = eigenvals3(d, e, num_evals)
 
     if eigenvectors:
         for i in range(num_evals):
-    	    evals[i], evecs[:, i] = inversePower3(d, c, 1.00000001*evals[i])
+    	    evals[i], evecs[:, i] = inversePower3(d, e, 1.00000001*evals[i])
 #
 #    An alternative that uses the brakets from lamRange:
 #
 #    if eigenvectors:
 #        for i in range(num_evals):
 #            s = (r[i] + r[i+1])/2.0
-#    	    evals[i], evecs[:, i] = inversePower3(d, c, s)
+#    	    evals[i], evecs[:, i] = inversePower3(d, e, s)
 #    else:
-#	evals = eigenvals3(d, c, num_evals)
+#	evals = eigenvals3(d, e, num_evals)
 
     return evals, evecs
